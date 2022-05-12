1a4f44b89738d373b8deccab53a1618a54fff1d4
unknown <admin@ADMORPO04.ncshta.local>
2022-05-12 16:00:53 +0300

    write csv and txt file

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 
 const path = require('path')
+const fs = require('fs')
+const dirNames = require('./data')
+const createCsvWriter = require('csv-writer').createArrayCsvWriter
 
-let data = [
-    "Иванов Иван Иванович",
-    "Ян Иван Алексеевич",
-    "Смирнов Петр Петрович",
-    "Охтов Михаил Зауэрович",
-    "Тамилова Алина Анатолевна"
-    ]
+const csvWriter = createCsvWriter({
+    header: ['ФИО'],
+    path: __dirname + "\\" + "testData.csv"
+})
+
+function fileHandlerAppend(body, message) {
+/* 
+    fs.appendFile('testFile.txt', body, (err) => {
+        if (err) throw err
+        console.log("txt" + message)
+    }) 
+*/
+
+    csvWriter.writeRecords(body)     
+    .then(() => {
+    console.log(message)
+    })
+
+}
+
+const app = async () => {
+
+/*  txt
+    for (let index = 0; index < dirNames.length; index++) {
+        fileHandlerAppend(dirNames[index] +  "\r\n", 'Data has been added!')
+    } 
+*/
+
+//csv
+
+const array = []
+
+for (let index = 0; index < dirNames.length; index++) {
     
-    let i = 1
-    
-    const count = () => {
-         for (let index = 0; index < data.length; index++) {
-            console.log(data[index], i, data[index].length)        
-            i++
-        } 
+   const content = await fs.readFileSync(path.join(__dirname, 'users', dirNames[index], "data.txt"), 'utf8', async (err, fileContent) => {
+        if(err) throw err   
+        return fileContent   
+    }) 
+    array.push([dirNames[index], content]) 
+
+    /*  */
+   
+    if(index + 1 === dirNames.length) {
+        fileHandlerAppend(array, 'The data has been added to the testData.csv file')
+        console.log(array)
     }
-    count()
+} 
+
+}
+
+app()
